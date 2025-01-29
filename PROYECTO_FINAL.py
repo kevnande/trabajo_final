f1818b508f65cb030346b36f9f5b46c844b240a5
kevnande <kevinfer26798@gmail.com>
2025-01-29 14:16:24 -0600

    Update PROYECTO_FINAL.py

--- PROYECTO_FINAL.py
+++ PROYECTO_FINAL.py
@@ -4,7 +4,7 @@ from google.cloud import firestore
 from google.oauth2 import service_account
 import json
 key_dict = json.loads(st.secrets["firebase"])
-creds = service_account.Credentials.from_service_account_info(key_dict)
+creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project="movies")
 
 @st.cache_data
